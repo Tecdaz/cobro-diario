dd1a3bc224cceeb7bcd8e5dc105188b53b8d4edc
Santiago Arias <santi99oca@gmail.com>
2025-02-14 17:23:27 -0300

    SearchBar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import CardCliente from "@/components/card_cliente/CardCliente";
+import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 
 export default function Home() {
   const clientes = Array(30).fill({ nombre: "Juan Perez", cuota: 1232, saldo: 1234, id: 1, status: "ok" });
   return (
     <div className="flex flex-col">
+      <SearchBar />
       {clientes.map((cliente, index) => (
         <CardCliente key={index} {...cliente} className={index % 2 === 0 && "bg-gray-300"} />
       ))}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,20 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+
+export default function SearchBar() {
+    return (
+        <div className="h-16 p-4 flex items-center gap-4 justify-between w-full">
+            <div className='flex gap-2 items-center'>
+                <input type="checkbox" />
+                <label>Vrf</label>
+            </div>
+            <div className="flex flex-1 gap-2 items-center p-2 bg-white rounded-lg w-full min-w-0">
+
+                <input className="flex-1 min-w-0 focus:outline-none" type="text" placeholder="Buscar" />
+                <button>
+                    <MagnifyingGlassIcon className='h-6 w-6 rotate-90' />
+                </button>
+            </div>
+        </div>
+
+    )
+}
