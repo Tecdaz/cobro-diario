c5e7ab49ffe20c159e20a9a5c1d7b256e8366fd7
Santiago Arias <santi99oca@gmail.com>
2025-02-13 22:36:29 -0300

    Datos de prueba

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,14 @@
+import CardCliente from "@/components/card_cliente/CardCliente";
 import Image from "next/image";
 
 export default function Home() {
+  const clientes = Array(30).fill({ nombre: "Juan Perez", cuota: 1232, saldo: 1234, id: 1, status: "ok" });
   return (
     <div className="flex flex-col">
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
-      <div className="h-14 bg-red-400"></div>
-      <div className="h-14 bg-pink-400"></div>
+      {clientes.map((cliente, index) => (
+        <CardCliente key={index} {...cliente} className={index % 2 === 0 && "bg-gray-300"} />
+      ))}
+
     </div>
   );
 }
