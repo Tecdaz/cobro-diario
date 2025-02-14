f4fce3cb8499c34c8622c738b937616005464acc
Santiago Arias <santi99oca@gmail.com>
2025-02-13 22:35:57 -0300

    Header y Navbar fijos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="flex gap-4 p-4 h-14 bg-orange-400 relative justify-center">
+        <div className="flex gap-4 p-4 h-10 bg-orange-400 justify-center fixed top-0 left-0 w-full items-center shadow-[0px_4px_8px_0px_rgba(0,0,0,0.2)]">
             <div className="absolute left-4">Menu</div>
             <div className="text-center">Titulo</div>
         </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,8 @@ import NavBarItem from "./NavBarItem"
 
 export default function NavBar() {
     return (
-        <div className="h-16 w-full flex justify-between items-center">
+        <div className="h-16 w-full flex justify-between items-center fixed bottom-0 left-0 
+        shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.2)]">
             <NavBarItem />
             <NavBarItem />
             <NavBarItem />
