b728c69346ef5abcfef1689b9cd42d8d30e1586b
Santiago Arias <santi99oca@gmail.com>
2025-02-14 17:23:16 -0300

    Iconos de la navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
-import NavBarItem from "./NavBarItem"
+import NavBarItem from "@/components/NavBarItem"
+import { RectangleStackIcon, UserPlusIcon, ScaleIcon, PresentationChartBarIcon } from "@heroicons/react/24/solid"
 
 export default function NavBar() {
     return (
         <div className="h-16 w-full flex justify-between items-center fixed bottom-0 left-0 
         shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.2)]">
-            <NavBarItem />
-            <NavBarItem />
-            <NavBarItem />
-            <NavBarItem />
+            <NavBarItem Icon={RectangleStackIcon} />
+            <NavBarItem Icon={UserPlusIcon} />
+            <NavBarItem Icon={ScaleIcon} />
+            <NavBarItem Icon={PresentationChartBarIcon} />
         </div>
     )
 }
--- src/components/NavBarItem.jsx
+++ src/components/NavBarItem.jsx
@@ -1,5 +1,8 @@
-export default function NavBarItem() {
+export default function NavBarItem(props) {
+    const { Icon } = props
     return (
-        <div className="h-full w-full bg-orange-400"></div>
+        <div className="h-full w-full bg-orange-400 flex justify-center items-center">
+            <Icon className="h-12 w-12" />
+        </div>
     )
 }
