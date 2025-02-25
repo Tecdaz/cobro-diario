af2d86b7816e8db5639a7b9d28490dbf062a78e0
Santiago Arias <santi99oca@gmail.com>
2025-02-25 00:40:46 -0300

    Input de tipo select

---
+++ src/components/SelectField.jsx
@@ -0,0 +1,20 @@
+export default function SelectField({ label, name, options, register, required, errors, value, handleOnChange, isDisabled }) {
+    return (
+        <div className="flex flex-col gap-1 text-base min-w-0">
+            <label className="text-sm">{label}</label>
+            <select
+                className="p-1 rounded-md border-2 min-w-0"
+                {...register(name, { required })}
+                value={value}
+                disabled={isDisabled}
+            >
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            {errors[name] && <span className="text-red-600">Este campo es requerido</span>}
+        </div>
+    );
+};
