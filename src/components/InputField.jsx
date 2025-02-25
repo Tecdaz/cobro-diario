880b9ab3dc9eb3f6ff8b50813fa10cd49c621c5f
Santiago Arias <santi99oca@gmail.com>
2025-02-25 00:41:16 -0300

    Refactorizacion de InputField

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,13 @@
 
-export default function InputField({ label, register, name, required, errors, isDisabled, value }) {
+export default function InputField({ label, register, name, required, errors, isDisabled, value, handleOnChange, type, className }) {
     return (
-        <div className="flex flex-col gap-1 text-base min-w-0">
+        <div className={`flex flex-col gap-1 text-base min-w-0 ${className ? className : ""}`}>
             <label className="text-sm">{label}</label>
-            <input className="p-1 rounded-md border-2 min-w-0" {...register(name, { required: required })} value={value} disabled={isDisabled} />
+            <input
+                className="p-1 rounded-md border-2 min-w-0" {...register(name, { required: required })} value={value}
+                disabled={isDisabled}
+                onChange={handleOnChange && ((e) => handleOnChange(e))}
+                type={type ? type : "text"} />
             {errors[name] && <span className="text-red-600">Este campo es requerido</span>}
         </div>
     )
