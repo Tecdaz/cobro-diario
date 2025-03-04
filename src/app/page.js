b9c3dd4f2929fe6d90bf2bec09898a637969963a
Santiago Arias <santi99oca@gmail.com>
2025-03-04 15:01:51 -0300

    El boton de vrf sale de la search bar

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,38 @@ import SearchBar from "@/components/SearchBar";
 import Image from "next/image";
 import { useLayout } from "@/contexts/LayoutContext";
 import { useEffect, useState } from "react";
-import { getTodayCobros } from "@/lib/db";
+import { getTodayCobros, getTodayPayments } from "@/lib/db";
+import Link from "next/link";
 
 export default function Home() {
   const { handleTitleChange } = useLayout();
   const [data, setData] = useState([]);
+  const [vrfActive, setVrfActive] = useState(false);
+
+  const handleVrfChange = () => {
+    setVrfActive(!vrfActive);
+    console.log("Vrf", !vrfActive);
+  }
+
+
   useEffect(() => {
     handleTitleChange("Clientes")
+
+  }, [handleTitleChange]);
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const dataFetch = await getTodayCobros();
-        console.log("Data", dataFetch);
-        setData(dataFetch);
+        if (vrfActive) {
+          const dataFetch = await getTodayPayments();
+          console.log("Data", dataFetch);
+          setData(dataFetch);
+        }
+        else {
+          const dataFetch = await getTodayCobros();
+          console.log("Data", dataFetch);
+          setData(dataFetch);
+        }
       }
       catch (error) {
         console.log("Error", error);
@@ -23,20 +43,26 @@ export default function Home() {
 
     }
     fetchData();
-  }, [handleTitleChange]);
+  }, [vrfActive]);
 
   return (
     <div className="flex flex-col">
-      <SearchBar />
+      <div className="h-16 p-4 flex items-center gap-4 justify-between w-full">
+        <div className='flex gap-2 items-center'>
+          <input onClick={handleVrfChange} type="checkbox" />
+          <label>Vrf</label>
+        </div>
+        <SearchBar />
+      </div>
       {data.map((venta, index) => (
-        <CardCliente
-          key={index}
-          className={index % 2 === 0 && "bg-gray-300"}
-          nombre={venta.nombre}
-          cuota={venta.valor_cuota}
-          saldo={10000} />
-      ))}
-
+        <Link href={`/ingresar_cuota/${venta.id}`} key={index}>
+          <CardCliente
+            key={index}
+            className={index % 2 === 0 && "bg-gray-300"}
+            nombre={venta.nombre}
+            cuota={venta.valor_cuota}
+            saldo={venta.saldo} />
+        </Link>))}
     </div>
   );
 }
