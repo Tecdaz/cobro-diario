0400ceaca474e56a68ad957ea7dcf49118b96317
Santiago Arias <santi99oca@gmail.com>
2025-03-09 14:03:06 -0300

    Cambios en el searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
     return (
-        <div className="flex flex-1 gap-2 items-center p-2 bg-white rounded-lg w-full min-w-0">
-            <input className="flex-1 min-w-0 focus:outline-none" type="text" placeholder="Buscar" />
-            <button>
-                <MagnifyingGlassIcon className='h-6 w-6' />
-            </button>
+        <div className="flex flex-1 gap-2 items-center p-2 bg-white rounded-lg w-full min-w-0 shadow-sm">
+            <input
+                className="flex-1 min-w-0 focus:outline-none"
+                type="text"
+                placeholder="Buscar por nombre, teléfono o dirección"
+                onChange={(e) => onSearch(e.target.value)}
+            />
+            <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
         </div>
     )
 }
