4e26c5095378aff8b371a352504bddf0c6458bb2
Santiago Arias <santi99oca@gmail.com>
2025-03-23 15:43:22 -0300

    Tamaño maximo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,20 +3,21 @@ import { RectangleStackIcon, UserPlusIcon, ScaleIcon, PresentationChartBarIcon }
 
 export default function NavBar() {
     return (
-        <div className="h-16 w-full flex justify-between items-center fixed bottom-0 left-0 
-        shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.2)]">
-            <NavBarItem href="/dashboard" >
-                <RectangleStackIcon className="h-12 w-12" />
-            </NavBarItem>
-            <NavBarItem href="/nueva_venta" >
-                <UserPlusIcon className="h-12 w-12" />
-            </NavBarItem>
-            <NavBarItem href="/gestion_gastos">
-                <ScaleIcon className="h-12 w-12" />
-            </NavBarItem>
-            <NavBarItem href="/resumen">
-                <PresentationChartBarIcon className="h-12 w-12" />
-            </NavBarItem>
+        <div className="h-16 w-full fixed bottom-0 left-0 shadow-[0px_-4px_8px_0px_rgba(0,0,0,0.2)] bg-orange-400 z-50">
+            <div className="mx-auto w-full max-w-[768px] h-full flex justify-between items-center">
+                <NavBarItem href="/dashboard" >
+                    <RectangleStackIcon className="h-12 w-12" />
+                </NavBarItem>
+                <NavBarItem href="/nueva_venta" >
+                    <UserPlusIcon className="h-12 w-12" />
+                </NavBarItem>
+                <NavBarItem href="/gestion_gastos">
+                    <ScaleIcon className="h-12 w-12" />
+                </NavBarItem>
+                <NavBarItem href="/resumen">
+                    <PresentationChartBarIcon className="h-12 w-12" />
+                </NavBarItem>
+            </div>
         </div>
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import { Search } from 'lucide-react';
 
 export default function SearchBar({ value, onChange, placeholder }) {
     return (
-        <div className="relative flex-1 max-w-md">
+        <div className="relative flex-1">
             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                 <Search className="h-5 w-5 text-gray-400" />
             </div>
